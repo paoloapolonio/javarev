8fcfe3a854dd5c72317b2b4dfe2fa37f034b5fd8
paolo <paolo143@yahoo.com>
2018-04-25 13:33:11 +0800

    mavenized

---
+++ src/main/java/mysamp/javarev/App.java
@@ -0,0 +1,13 @@
+package mysamp.javarev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
